eec8246644dfefa9fff8428bf7a4210ecf59a537
josibake <josibake@protonmail.com>
2023-08-15 20:55:19 +0200

    add exponential_backoff decorator
    there are a lot of race conditions when setting up the containers, and
    I imagine we will run into more as we start writing scenarios, so this is
    a good thing to have in the toolbox
    
    closes #9

---
+++ src/warnet/utils.py
@@ -0,0 +1,27 @@
+import time
+import functools
+
+def exponential_backoff(max_retries=5, base_delay=1, max_delay=32):
+    """
+    A decorator for exponential backoff.
+
+    Parameters:
+    - max_retries: Maximum number of retries before giving up.
+    - base_delay: Initial delay in seconds.
+    - max_delay: Maximum delay in seconds.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    retries += 1
+                    if retries == max_retries:
+                        raise e
+                    delay = min(base_delay * (2 ** retries), max_delay)
+                    time.sleep(delay)
+        return wrapper
+    return decorator
