90331e6b31cba9a9a2a679c08f8a01a89411f868
josibake <josibake@protonmail.com>
2023-08-18 14:59:23 +0200

    super messy warnet commit

--- src/warnet/connect_nodes.py
+++ src/warnet/connect_nodes.py
@@ -8,15 +8,43 @@ from .docker_utils import get_container_ip, get_containers
 
 logging.basicConfig(level=logging.INFO)
 
+def create_graph_with_probability(num_nodes, p):
+    graph = nx.erdos_renyi_graph(num_nodes, p, directed=True)
+    for node in graph.nodes():
+        graph.nodes[node]['version'] = '25.0'
+    return graph
 
-def connect_edges(client: docker.DockerClient, graph_file: str):
+
+def generate_topology_with_probability(client, num_nodes, p):
+    g = create_graph_with_probability(num_nodes, p)
+    generate_topology(client, g)
+
+def generate_topology(client: docker.DockerClient, g):
+    """
+    Creates a random network using an erdos-renyi model.
+
+    :param client: docker client
+    :param num_nodes: number of nodes
+    :param p: probability of a connection to be created
+    :return:
+    """
+
+    logging.info("Creating scenario with a random topology: {} nodes and {} edges".format(len(g.nodes()), g.number_of_edges()))
+    connect_edges(client, g)
+
+def read_graph_from_file(graph_file: str):
+    try:
+        return nx.read_graphml(graph_file, node_type=int)
+    except Exception as e:
+        logging.error(f"An error occurred while reading {graph_file}: {e}")
+
+def connect_edges(client: docker.DockerClient, graph):
     """
     Setup and add nodes to the network.
 
     :param graph_file: The path to the graph file
     """
     try:
-        graph = nx.read_graphml(graph_file, node_type=int)
         logging.info(get_containers(client))
         for edge in graph.edges():
             source = f"warnet_{str(edge[0])}"
@@ -26,3 +54,8 @@ def connect_edges(client: docker.DockerClient, graph_file: str):
             addpeeraddress(source_container, get_container_ip(client, dest))
     except Exception as e:
         logging.error(f"An error occurred while setting up the network: {e}")
+
+def generate_topology_from_file(client: docker.DockerClient, graph_file: str):
+    graph = read_graph_from_file(graph_file)
+    generate_topology(client, graph)
+
--- src/warnet/main.py
+++ src/warnet/main.py
@@ -1,19 +1,108 @@
+import typer
+import sys
+import pkgutil
+import os
+import subprocess
+from datetime import datetime
 import logging
 import docker
 import warnet
+from warnet.connect_nodes import *
+from warnet.client import *
 
-BITCOIN_GRAPH_FILE = './graphs/basic3.graphml'
+app = typer.Typer()
 
-logging.basicConfig(level=logging.INFO)
+@app.command()
+def bcli(node: int, method: str, params: list[str] = typer.Option([])):
+    """
+    On the battlefield of node {node}, we dispatch the command '{method}' with the marching orders '{params}'.
+    """
+    try:
+        result = get_bitcoin_cli(node, method, params)
+        typer.echo(result)
+    except Exception as e:
+        typer.echo(f"In our quest to command node {node}, we encountered resistance: {e}")
 
+@app.command()
+def log(node: int):
+    """
+    From the archives of node {node}, we shall unveil the tales and chronicles of its endeavors.
+    """
+    try:
+        result = get_debug_log(node)
+        typer.echo(result)
+    except Exception as e:
+        typer.echo(f"In our pursuit of knowledge from node {node}, we were thwarted: {e}")
 
-def main():
-    client = docker.from_env()
-    warnet.delete_containers(client)
-    warnet.generate_docker_compose(BITCOIN_GRAPH_FILE)
-    warnet.docker_compose()
-    warnet.connect_edges(client, BITCOIN_GRAPH_FILE)
+@app.command()
+def messages(src: int, dst: int):
+    """
+    Relaying the dispatches exchanged between the strongholds of nodes {src} and {dst}.
+    """
+    try:
+        messages = get_messages(src, dst)
+        out = ""
+        for m in messages:
+            timestamp = datetime.utcfromtimestamp(m["time"] / 1e6).strftime('%Y-%m-%d %H:%M:%S')
+            direction = "Advance" if m["outbound"] else "Retreat"
+            body = m.get("body", "")
+            out += f"At the hour of {timestamp}, the order was to '{direction}' bearing the message '{m['msgtype']} {body}'\n"
+        typer.echo(out)
+    except Exception as e:
+        typer.echo(f"Amidst the fog of war, we failed to relay messages between strongholds {src} and {dst}: {e}")
+
+@app.command()
+def run(scenario_name: str, args: list[str] = typer.Option([])):
+    """
+    With valor and courage, we embark on the mission named '{scenario_name.py}', armed with the strategies '{args}'.
+    """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    mod_path = os.path.join(dir_path, '..', 'scenarios', f"{scenario_name}.py")
+    run_cmd = [sys.executable, mod_path] + args
+    subprocess.run(run_cmd)
+
+@app.command()
+def stop():
+    """
+    In the face of overwhelming odds, we make the strategic decision to halt our operations and regroup.
+    """
+    try:
+        result = stop_network()
+        typer.echo(result)
+    except Exception as e:
+        typer.echo(f"As we endeavored to cease operations, adversity struck: {e}")
+
+@app.command()
+def generate(nodes: int, probability: float):
+    """
+    With valor and foresight, we shall forge a battlefield of {nodes} strongholds, where alliances are formed with a probability of {probability}.
+    """
+    try:
+        client = docker.from_env()
+        warnet.delete_containers(client)
+        graph = create_graph_with_probability(nodes, probability)
+        warnet.generate_docker_compose(graph)
+        warnet.docker_compose()
+        generate_topology(client, graph)
+        typer.echo(f"A grand theater of war with {nodes} strongholds has been crafted, with alliances formed at the whims of chance, set at a probability of {probability}.")
+    except Exception as e:
+        typer.echo(f"While crafting our theater of war with {nodes} strongholds, we faced an unexpected challenge: {e}")
+
+@app.command()
+def load(file: str):
+    """
+    With the map of {file} in hand, we shall lay out our strategy and position our forces accordingly.
+    """
+    try:
+        client = docker.from_env()
+        warnet.delete_containers(client)
+        warnet.generate_docker_compose(file)
+        warnet.docker_compose()
+        generate_topology_from_file(client, file)
+        typer.echo(f"The battle plans from {file} have been unfurled and our strategy is clear.")
+    except Exception as e:
+        typer.echo(f"In our endeavor to decipher the plans from {file}, we encountered a setback: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
