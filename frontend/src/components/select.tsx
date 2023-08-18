9b332f69e095d69d84a0c7e5bcc9431dd38c3bec
Theophilus <isahtheophilus@gmail.com>
2023-08-18 11:40:03 +0200

    feat: add prebuilt node components

---
+++ frontend/src/components/select.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+
+import { defaultEdgesData, defaultNodesData } from "@/app/data";
+import { useNodeGraphContext } from "@/contexts/node-graph-context";
+import { NodePersonaType } from "@/types";
+import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import * as Select from "@radix-ui/react-select";
+
+const SelectBox = () => {
+  const { setNodePersonaFunc, addNode, setNodeEdges } = useNodeGraphContext();
+
+  const setNodePersona = (value: NodePersonaType) => {
+    setNodePersonaFunc(value as NodePersonaType);
+    if (value === "prebuilt") {
+      addNode(defaultNodesData);
+      setNodeEdges(defaultEdgesData);
+    }
+  };
+
+  return (
+    <Select.Root
+      onValueChange={(value) => {
+        setNodePersona(value as NodePersonaType);
+      }}
+    >
+      <Select.Trigger
+        className="w-[280px] border inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-x-[20px] bg-white text-slate-900 shadow-[0_2px_10px] shadow-black/10 hover:bg-slate-50 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
+        aria-label="Node topology"
+      >
+        <Select.Value placeholder="Select a node topology..." />
+        <Select.Icon className="text-slate-900">
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal className="mt-[50px]">
+        <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+          <Select.Viewport className="p-[5px]">
+            <Select.Group className="flex flex-col gap-y-1">
+              <Select.Label className="px-[25px] text-xs leading-[25px] text-black">
+                Node personas
+              </Select.Label>
+              <Select.Item
+                value="custom"
+                className="relative text-[13px] leading-none text-violet-500 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] select-none data-[disabled]:text-green-300 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet-50 data-[highlighted]:text-violet-600"
+              >
+                <Select.ItemText className="text-black text-lg">
+                  Create your own node topology
+                </Select.ItemText>
+                <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+                  <CheckIcon />
+                </Select.ItemIndicator>
+              </Select.Item>
+              <Select.Item
+                value="prebuilt"
+                className="text-[13px] leading-none text-violet-500 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-green-300 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet-50 data-[highlighted]:text-violet-600"
+              >
+                <Select.ItemText className="text-black text-lg">
+                  Use a pre-built node topology
+                </Select.ItemText>
+                <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+                  <CheckIcon />
+                </Select.ItemIndicator>
+              </Select.Item>
+            </Select.Group>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
+
+export default SelectBox;
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
 const CANVAS_WIDTH = 650 as const;
 const CANVAS_HEIGHT = 650 as const;
 const LINK_DISTANCE = 150 as const;
+const NODE_ATTACHMENT_POINT = 10 as const;
 
 const BITCOIN_CORE_BINARY_VERSIONS = [
   "22.0",
@@ -10,9 +11,19 @@ const BITCOIN_CORE_BINARY_VERSIONS = [
   "25.0",
 ] as const;
 
+const NODE_LATENCY = [
+  "0ms",
+  "10ms",
+  "20ms",
+  "30ms",
+  "40ms",
+]
+
 export {
   CANVAS_WIDTH,
   CANVAS_HEIGHT,
   LINK_DISTANCE,
+  NODE_LATENCY,
+  NODE_ATTACHMENT_POINT,
   BITCOIN_CORE_BINARY_VERSIONS,
 };
