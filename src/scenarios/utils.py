f19058d06591f38fefc59f44ff9d677ff4f981fd
Matthew Zipkin <pinheadmz@gmail.com>
2023-08-21 22:13:12 -0400

    scenarios: ensure miner ensures descriptors

--- src/scenarios/utils.py
+++ src/scenarios/utils.py
@@ -1,5 +1,5 @@
 def ensure_miner(node):
     wallets = node.listwallets()
     if "miner" not in wallets:
-        node.createwallet("miner")
+        node.createwallet("miner", descriptors=True)
     return node.get_wallet_rpc("miner")
