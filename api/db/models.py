99a2c1973992980233507b115811e137bab01453
Theophilus <isahtheophilus@gmail.com>
2023-08-24 11:40:26 +0200

    chore: add db folder

---
+++ api/db/models.py
@@ -0,0 +1,29 @@
+import datetime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    createdAt = Column(DateTime, default=datetime.datetime.utcnow)
+    updatedAt = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+    networks = relationship("Network", back_populates="owner")
+
+class Network(Base):
+    __tablename__ = "networks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    status = Column(Boolean, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    createdAt = Column(DateTime, default=datetime.datetime.utcnow)
+    updatedAt = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+    owner = relationship("User", back_populates="networks")
