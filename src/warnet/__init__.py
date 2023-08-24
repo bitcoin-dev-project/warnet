363fab1331896e6ce8b411b8c2c6b49e0d193e73
willcl-ark <will@256k1.dev>
2023-08-24 13:31:39 +0200

    switch cli to typer

--- src/warnet/__init__.py
+++ src/warnet/__init__.py
@@ -1,4 +1,5 @@
 from .connect_nodes import connect_edges
+from .client import *
 from .docker_utils import delete_containers, get_containers, get_container_ip
 from .docker_compose import generate_docker_compose
 from .docker_utils import docker_compose
