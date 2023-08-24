9c600ba792f2863655079e108ecd2cbfb396abcf
Bigmarh <lamar.wilson@gmail.com>
2023-08-24 11:14:46 -0400

    Chore:Check email for DNS deliverability

--- api/validation/email.py
+++ api/validation/email.py
@@ -1,12 +1,19 @@
-from email_validator import validate_email, EmailNotValidError
+import email_validator
+email_validator.SPECIAL_USE_DOMAIN_NAMES.remove("test")
+email_validator.CHECK_DELIVERABILITY=True
 from decouple import config
 
 
+def is_dev_env():
+	if config('ENVIROMENT') == "development":
+		return True;
+	return False;
+
 def _validate_email(email):
 	try:
-		emailinfo = validate_email(email,check_deliverability=False)
+		emailinfo = email_validator.validate_email(email,test_environment=is_dev_env())
 		email = emailinfo.normalized
 		return email, False
 
-	except EmailNotValidError as e:
+	except email_validator.EmailNotValidError as e:
 		return str(e), True
