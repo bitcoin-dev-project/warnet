2a7ddf623f5db5be9ba871b568d2a2bc3275381a
Theophilus <isahtheophilus@gmail.com>
2023-08-24 11:38:11 +0200

    chore: add db operations

--- api/README.md
+++ api/README.md
@@ -2,6 +2,17 @@
 
 To start the server run:
 
+1. Run the docker daemon
+
 ```bash
 docker-compose -f api-compose.yml up -d
 ```
+
+2. We use Postgresql as a data store. You can access it locally by using this connection string.
+
+```bash
+postgresql://warnet:password@db:5432/warnet
+```
+
+The server should be running on `localhost:8080`.
+To see all rest endpoints, check the API documentation at `localhost:8080/docs`
--- api/main.py
+++ api/main.py
@@ -1,27 +1,52 @@
-from typing import Union
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI
+from .db import crud, models, schemas, database
+
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-app = FastAPI(
-    title="Warnet API",
-    description="Warnet API",
-    version="0.0.1",
-)
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def read_root():
-    return {"Hello Viking": "Welcome to Warnet!"}
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.create_user(db, user)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return db_user
 
-@app.post("/warnet")
-def read_root():
-    return {"Hello": "warnet"}
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-@app.get("/users/me")
-def read_user_me():
-    return {"user_id": "the current user"}
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="email or password is incorrect")
+    return db_user
 
-@app.get("/user/{user_id}")
-def read_item(user_id: int, q: Union[str, None] = None):
-    return {"user_id": user_id, "q": q}
+@app.post("/user", response_model=schemas.User)
+def read_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="email or password is incorrect")
+    return db_user
+
+@app.post("/users/{user_id}/networks/", response_model=schemas.Network)
+def create_network_for_user(
+    user_id: int, network: schemas.NetworkCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_network(db=db, network=network, user_id=user_id)
+
+@app.get("/networks/", response_model=list[schemas.Network])
+def read_networks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    netwworks = crud.get_networks(db, skip=skip, limit=limit)
+    return netwworks
