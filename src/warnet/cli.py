88f6e30d5ba8419e7945b65b608aad49489c14d6
willcl-ark <will@256k1.dev>
2023-08-29 23:07:52 +0100

    fixups to cli and daemon interactions

---
+++ src/warnet/cli.py
@@ -0,0 +1,152 @@
+import requests
+from typing_extensions import Annotated
+from typing import Optional, Any, Tuple, Dict, Union
+from pathlib import Path
+
+from jsonrpcclient import Ok, parse, request
+import typer
+from rich import print
+import requests
+
+from templates import TEMPLATES
+from warnet.warnetd import WARNETD_PORT
+
+EXAMPLE_GRAPH_FILE = TEMPLATES / "example.graphml"
+
+cli = typer.Typer()
+
+
+def rpc(rpc_method, params: Optional[Union[Dict[str, Any], Tuple[Any, ...]]]):
+    payload = request(rpc_method, params)
+    response = requests.post(f"http://localhost:{WARNETD_PORT}/api", json=payload)
+    parsed = parse(response.json())
+
+    if isinstance(parsed, Ok):
+        return parsed.result
+    else:
+        print(parsed)
+        raise Exception(parsed.message)
+
+
+@cli.command()
+def bcli(
+    node: int,
+    method: str,
+    params: Annotated[Optional[list[str]], typer.Argument()] = None,
+    network: str = "warnet",
+):
+    """
+    Call bitcoin-cli <method> <params> on <node> in <network>
+    """
+    try:
+        result = rpc(
+            "bcli",
+            {"network": network, "node": node, "method": method, "params": params},
+        )
+        print(result)
+    except Exception as e:
+        print(f"bitcoin-cli {method} {params} failed on node {node}:\n{e}")
+
+
+@cli.command()
+def debug_log(node: int, network: str = "warnet"):
+    """
+    Fetch the Bitcoin Core debug log from <node> in <network>
+    """
+    try:
+        result = rpc("debug_log", {"node": node, "network": network})
+        print(result)
+    except Exception as e:
+        print(f"In our pursuit of knowledge from node {node}, we were thwarted: {e}")
+
+
+@cli.command()
+def messages(node_a: int, node_b: int, network: str = "warnet"):
+    """
+    Fetch messages sent between <node_a> and <node_b> in <network>
+    """
+    try:
+        result = rpc(
+            "messages", {"network": network, "node_a": node_a, "node_b": node_b}
+        )
+        print(result)
+    except Exception as e:
+        print(
+            f"Amidst the fog of war, we failed to relay messages between strongholds {node_a} and {node_b}: {e}"
+        )
+
+
+@cli.command()
+def list():
+    """
+    List available scenarios in the Warnet Test Framework
+    """
+    try:
+        result = rpc("list", None)
+        print(result)
+    except Exception as e:
+        print(f"Error listing scenarios: {e}")
+
+
+@cli.command()
+def run(scenario: str):
+    """
+    Run <scenario> from the Warnet Test Framework
+    """
+    try:
+        res = rpc("run", {"scenario": scenario})
+        print(res)
+    except Exception as e:
+        print(f"Error running scenario: {e}")
+
+
+@cli.command()
+def start(graph_file: Path = EXAMPLE_GRAPH_FILE, network: str = "warnet"):
+    """
+    Start a warnet with topology loaded from a <graph_file> into [network] (default: "warnet")
+    """
+    try:
+        result = rpc("from_file", {"graph_file": str(graph_file), "network": network})
+        print(result)
+    except Exception as e:
+        print(f"Error creating network: {e}")
+
+
+@cli.command()
+def stop(network: str = "warnet"):
+    """
+    Stop all docker containers in [network] (default: "warnet").
+    """
+    try:
+        result = rpc("stop", {"network": network})
+        print(result)
+    except Exception as e:
+        print(f"As we endeavored to cease operations, adversity struck: {e}")
+
+
+@cli.command()
+def wipe(network: str = "warnet"):
+    """
+    Stop and then erase all docker containers in [network] (default: "warnet"), and then the docker network itself.
+    """
+    try:
+        result = rpc("wipe", {"network": network})
+        print(result)
+    except Exception as e:
+        print(f"Error wiping network: {e}")
+
+
+@cli.command()
+def stop_daemon():
+    """
+    Stop the warnetd daemon.
+    """
+    try:
+        result = rpc("stop_daemon", None)
+        print(result)
+    except Exception as e:
+        print(f"As we endeavored to cease operations, adversity struck: {e}")
+
+
+if __name__ == "__main__":
+    cli()
--- src/warnet/warnetd.py
+++ src/warnet/warnetd.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask
-from flask_jsonrpc import JSONRPC
+from flask_jsonrpc.app import JSONRPC
 
 import scenarios
 from warnet.warnet import Warnet
@@ -21,6 +21,7 @@ from warnet.client import (
 )
 
 WARNETD_PORT = 9276
+RPC_TIMEOUT = 600
 continue_running = True
 
 app = Flask(__name__)
@@ -33,20 +34,24 @@ log_file_path = os.path.join(xdg_state_home, 'warnet', 'warnet.log')
 # Ensure the directory exists
 os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
-logger = logging.getLogger('warnetd')
-logger.setLevel(logging.DEBUG)
-# Create a handler that writes log messages to a file, with a maximum
-# log file size of 1 MB, keeping 3 backup old log files.
-handler = RotatingFileHandler(log_file_path, maxBytes=1_000_000, backupCount=3)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+# Configure root logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[
+        RotatingFileHandler(log_file_path, maxBytes=1_000_000, backupCount=3, delay=True)
+    ],
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+# Disable urllib3.connectionpool logging
+logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
+
+logger = logging.getLogger("warnetd")
 
 
 @jsonrpc.method('bcli')
-def bcli(network: str, node: int, method: str, params: list[str] = []):
+def bcli(node: int, method: str, params: list[str] = [], network: str = "warnet"):
     """
-    Call bitcoin-cli <method> <params> on <node> in [network]
+    Call bitcoin-cli on <node> <method> <params> in [network]
     """
     try:
         result = get_bitcoin_cli(network, node, method, params)
@@ -102,26 +107,26 @@ def list() -> list[str]:
                 sc.append(f"{s.name.ljust(20)}, {m.cli_help()}")
         return sc
     except Exception as e:
-        raise Exception(f"{e}")
+        return [f"Exception {e}"]
 
 @jsonrpc.method('run')
-def run(scenario: str):
+def run(scenario: str, network: str = "warnet") -> str:
     """
     Run <scenario> from the Warnet Test Framework
     """
     try:
-        # TODO: should accept network argument
+        # TODO: should handle network argument
         dir_path = os.path.dirname(os.path.realpath(__file__))
         mod_path = os.path.join(dir_path, '..', 'scenarios', f"{sys.argv[2]}.py")
         run_cmd = [sys.executable, mod_path] + sys.argv[3:]
         subprocess.run(run_cmd, shell=False)
-        return True
+        return f"Running scenario {scenario}..."
     except Exception as e:
-        raise Exception(f"{e}")
+        return f"Exception {e}"
 
 
 @jsonrpc.method()
-def from_file(graph_file: str, network: str):
+def from_file(graph_file: str, network: str = "warnet") -> str:
     """
     Run a warnet with topology loaded from a <graph_file>
     """
@@ -133,38 +138,34 @@ def from_file(graph_file: str, network: str):
         wn.docker_compose_up()
         wn.apply_network_conditions()
         wn.connect_edges()
-        return True
+        return f"Created warnet named '{network}' from graph file {graph_file}"
     except Exception as e:
-        raise Exception(f"{e}")
+        return f"Exception {e}"
 
 
 @jsonrpc.method('stop')
-def stop() -> bool:
+def stop(network: str = "warnet") -> str:
     """
     Stop all docker containers in <network>.
     """
     try:
-        result = stop_network()
-        return result
+        result = stop_network(network)
+        return "Warnet stopped" if result else "Warnet not stopped"
     except Exception as e:
-        raise Exception(f"{e}")
+        return f"Exception {e}"
 
 
 @jsonrpc.method('wipe')
-def wipe() -> bool:
+def wipe(network: str = "warnet") -> str:
     """
     Stop and then erase all docker containers in <network>, and then the docker network itself.
     """
     try:
-        stop_network()
-        result = wipe_network()
+        result = "Warnet stopped" if stop_network(network) else "Warnet not stopped"
+        result += "\nWarnet wiped" if wipe_network(network) else "Warnet not wiped"
         return result
     except Exception as e:
-        raise Exception(f"{e}")
-
-
-def run_server():
-    app.run(port=WARNETD_PORT, threaded=True)
+        return f"Exception {e}"
 
 
 @jsonrpc.method('stop_daemon')
@@ -176,12 +177,17 @@ def stop_daemon() -> str:
     return "Stopping daemon..."
 
 
+def run_server():
+    app.run(port=WARNETD_PORT, threaded=True)
+
+
 def run_gunicorn():
     subprocess.run([
-        "gunicorn", 
-        "-w", "4", 
+        "gunicorn",
+        "-w", "4",
         f"-b :{WARNETD_PORT}",
-        "--daemon", 
+        "--daemon",
+        f"-t {RPC_TIMEOUT}",
         "--log-level", "debug",
         "--access-logfile", log_file_path,
         "--error-logfile", log_file_path,
