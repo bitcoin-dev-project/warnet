41c0efac6f9437b7fdff6c5d3444b16b7001aa9e
willcl-ark <will@256k1.dev>
2023-09-04 14:13:57 +0100

    fix scenarios

--- src/services/node_exporter.py
+++ src/services/node_exporter.py
@@ -6,7 +6,7 @@ class NodeExporter(BaseService):
         super().__init__(docker_network)
         self.service = {
             "image": "prom/node-exporter:latest",
-            "container_name": "node-exporter",
+            "container_name": f"{self.docker_network}_node-exporter",
             "volumes": ["/proc:/host/proc:ro", "/sys:/host/sys:ro", "/:/rootfs:ro"],
             "command": ["--path.procfs=/host/proc", "--path.sysfs=/host/sys"],
             "networks": [self.docker_network],
