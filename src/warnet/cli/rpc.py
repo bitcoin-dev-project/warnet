379659152db77eee0316acb85ebc16b3ddb2d769
Matthew Zipkin <pinheadmz@gmail.com>
2023-09-06 14:30:41 -0400

    cli: remove logging from rpc
    output was being doubled by print() statements in calling functions

--- src/warnet/cli/rpc.py
+++ src/warnet/cli/rpc.py
@@ -1,18 +1,13 @@
-import logging
 import requests
 from jsonrpcclient.responses import Ok, parse
 from jsonrpcclient.requests import request
 from typing import Any, Dict, Tuple, Union, Optional
 from warnet.warnetd import WARNETD_PORT
 
-logger = logging.getLogger(__name__)
-
 
 def rpc_call(rpc_method, params: Optional[Union[Dict[str, Any], Tuple[Any, ...]]]):
     payload = request(rpc_method, params)
-    logger.debug(f"Constructed rpc call: {payload}")
     response = requests.post(f"http://localhost:{WARNETD_PORT}/api", json=payload)
-    logger.debug(f"RPC respose: {response.status_code}, {response.text}")
     parsed = parse(response.json())
 
     if isinstance(parsed, Ok):
