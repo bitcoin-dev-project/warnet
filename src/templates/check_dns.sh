d375b7c4f9069fd2204bdba22fd1ffa5263b96e5
willcl-ark <will@256k1.dev>
2023-09-08 12:43:17 +0100

    print DNS reponse for logging

--- src/templates/check_dns.sh
+++ src/templates/check_dns.sh
@@ -9,12 +9,18 @@ MAX_RETRIES=100
 RETRY_COUNT=0
 SLEEP=1
 
+# Loop until maximum retries reached
 while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+  # Increment the retry count
   RETRY_COUNT=$((RETRY_COUNT+1))
- 
-  # Query the DNS server and check for any lines in the "ANSWER SECTION" with an IP address
-  if dig @$DNS_SERVER $HOSTNAME +noall +answer | grep -qE 'IN\s+A\s+\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'; then
-    echo "Got response from DNS server"
+  
+  # Query the DNS server and store the answer section in a variable
+  RESPONSE=$(dig @$DNS_SERVER $HOSTNAME +noall +answer)
+
+  # Check if the response contains any IP addresses
+  if echo "$RESPONSE" | grep -qE 'IN\s+A\s+\b([0-9]{1,3}\.){3}[0-9]{1,3}\b'; then
+    echo "Received valid DNS response:"
+    echo "$RESPONSE"
     exit 0
   else
     sleep $SLEEP
