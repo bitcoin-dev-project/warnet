bbdeeb44d4f8da70e491fb3bf3fdc9be0a35ef44
IgboPharaoh <solomoneze62@gmail.com>
2023-09-08 14:07:37 +0100

    style: add font family

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter, IBM_Plex_Mono } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+const ibm_plex_mono = IBM_Plex_Mono({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['400', '700', '500'],
+});
 
 export const metadata: Metadata = {
-  title: 'Warnet',
-  description: 'Warnet',
-}
+    title: 'Warnet',
+    description: 'Warnet',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang='en' className={ibm_plex_mono.className}>
+            <body>{children}</body>
+        </html>
+    );
 }
