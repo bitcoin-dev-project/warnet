ea93c789eb6f5a47d6f2451ec6ab1aac6b4eed8f
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-09-11 17:07:21 +0100

    fix: added the home page back

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,7 @@
 'use client';
+import React from 'react';
+import HomePage from '@/components/home';
 
-import React from "react";
-import NetworkDialog from "@/components/init-dialog";
-import { NetworkProvider } from "@/contexts/network-context";
-import ReactFlowGraph from "@/components/react-flow-graph";
-import "reactflow/dist/style.css";
-import { NodeGraphFlowProvider } from "@/contexts/node-flow-context";
 export default function Home() {
-  return (
-    <NetworkProvider>
-      <NodeGraphFlowProvider>
-        <main className="bg-black flex min-h-screen h-[100vh] items-center justify-center">
-          <NetworkDialog />
-          <ReactFlowGraph />
-        </main>
-      </NodeGraphFlowProvider>
-    </NetworkProvider>
-  );
+  return <HomePage />;
 }
