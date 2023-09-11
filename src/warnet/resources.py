c93fc1beb7f8d65c840f389f5f9f337e07c891bb
willcl-ark <will@256k1.dev>
2023-09-11 15:49:19 +0100

    add container resource profiles
    Adds a few demo profiles such as laptop, desktop, raspberry_pi, server to the graph config.
    
    While we are using docker-compose, and not swarm, we cannot easily limit physical CPUS (withough downgrading to docker-compose v2), but we can get "relative resourse usage", and memory limitations.

---
+++ src/warnet/resources.py
@@ -0,0 +1,23 @@
+# resource profile presets
+resource_profiles = {
+    "default": {
+        "cpus": "4",
+        "memory": "4G",
+    },
+    "raspberry_pi": {
+        "cpus": "2",
+        "memory": "2G",
+    },
+    "laptop": {
+        "cpus": "4",
+        "memory": "8G",
+    },
+    "desktop": {
+        "cpus": "8",
+        "memory": "16G",
+    },
+    "server": {
+        "cpus": "16",
+        "memory": "32G",
+    },
+}
