c8fc87519e2ccd783df16df1cdb27101e573759b
Emmanuel-Develops <itakpeemma@gmail.com>
2023-09-14 19:49:29 +0100

    feat: refactor download behaviour to that of generate graph

--- frontend/src/helpers/download-graphml.ts
+++ frontend/src/helpers/download-graphml.ts
@@ -1,11 +1,21 @@
 
 const graphmlDIR = "graphml"
 const downloadGraph = async (path: string) => {
-  console.log({path})
-  const a = document.createElement("a");
-  a.href = `${graphmlDIR}/${path}`;
-  a.download = path;
-  a.click();
+  try {
+    const response = await fetch(graphmlDIR + "/" + path);
+    if (!response.ok) {
+      throw new Error('Failed to fetch the XML file');
+    }
+    const xml = await response.text();
+    const blob = new Blob([xml], { type: "application/xml" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = path;
+    a.click();
+  } catch(error) {
+    console.error(error)
+  }
 }
 
 export default downloadGraph
