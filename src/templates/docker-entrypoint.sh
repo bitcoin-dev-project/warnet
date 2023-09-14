092c95b602106cf39d4ab8c92534238ebe146c52
willcl-ark <will@256k1.dev>
2023-09-14 13:13:07 +0100

    remove dns liveness check
    This can be a bottleneck for scaling

--- src/templates/docker-entrypoint.sh
+++ src/templates/docker-entrypoint.sh
@@ -38,9 +38,6 @@ if [ "$(echo "$1" | cut -c1)" = "-" ] || [ "$1" = "bitcoind" ]; then
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
-# Wait for the dns-seed to start returning results
-./check_dns.sh
-
 if [ "$1" = "bitcoind" ]; then
   if [ -n "$BITCOIND_ARGS" ]; then
     echo "$0: appending BITCOIND_ARGS to bitcoind command"
