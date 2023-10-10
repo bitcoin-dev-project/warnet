9dbbd9936cdf0b84f77bb22d3004dcdb6e644318
Samson Olusegun <olusegunsamson@live.com>
2023-10-10 18:27:38 +0100

    config: extract db url to secret

--- client-api/db/database.py
+++ client-api/db/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-DATABASE_URL = "postgresql://warnet:password@db:5432/warnet"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(config('DATABASE_URL'))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
