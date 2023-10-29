d30d4cc0076a60ca3dc98b3bb8308fec1ab3bde0
willcl-ark <will@256k1.dev>
2023-10-29 11:46:36 +0000

    re-enable grafana

--- src/interfaces/docker_interface.py
+++ src/interfaces/docker_interface.py
@@ -13,6 +13,7 @@ from .interfaces import ContainerInterface
 from warnet.utils import bubble_exception_str, parse_raw_messages
 from services.cadvisor import CAdvisor
 from services.fork_observer import ForkObserver
+from services.grafana import Grafana
 from services.tor import Tor
 from services.prometheus import Prometheus
 from templates import TEMPLATES
@@ -252,7 +253,7 @@ class DockerInterface(ContainerInterface):
             # grep: disable-exporters
             Prometheus(warnet.network_name, self.config_dir),
             # NodeExporter(warnet.network_name),
-            # Grafana(warnet.network_name),
+            Grafana(warnet.network_name),
             CAdvisor(warnet.network_name, TEMPLATES),
             Tor(warnet.network_name, TEMPLATES),
             ForkObserver(warnet.network_name, warnet.fork_observer_config),
