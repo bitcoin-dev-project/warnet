b5423f491556b795525bc103bae61204aed331b7
Max Edwards <youwontforgetthis@gmail.com>
2023-11-22 16:41:30 +0000

    fix ln test

--- src/warnet/test_framework_bridge.py
+++ src/warnet/test_framework_bridge.py
@@ -45,8 +45,8 @@ class WarnetTestFramework(BitcoinTestFramework):
         ch.setFormatter(formatter)
         self.log.addHandler(ch)
 
-        warnet = Warnet.from_network(self.options.network, backend="docker")
-        for i, tank in enumerate(warnet.tanks):
+        self.warnet = Warnet.from_network(self.options.network, backend="docker")
+        for i, tank in enumerate(self.warnet.tanks):
             ip = tank.ipv4
             self.log.info(f"Adding TestNode {i} from {tank.container_name} with IP {ip}")
             node = TestNode(
