faf0eb0db61cac2312cdb1cb44b9e6d7aa5496a4
willcl-ark <will@256k1.dev>
2024-01-30 20:35:01 +0000

    scripts: fixup k8s rpc builder
    * cleanup buildx builder after building
    * write proper tag in the manifest

--- scripts/build-k8s-rpc.sh
+++ scripts/build-k8s-rpc.sh
@@ -7,15 +7,15 @@
 set -ex
 
 # Create a new builder to enable building multi-platform images
-docker buildx create --use
+BUILDER_NAME="warnet-rpc-builder"
+docker buildx create --name "$BUILDER_NAME" --use
 
 # Read DOCKER_REGISTRY from the environment
 : "${DOCKER_REGISTRY?Need to set DOCKER_REGISTRY}"
 : "${TAG?Need to set TAG}"
 
 # Architectures for building
-ARCHS=("amd64")
-# ARCHS=("amd64" "arm64" "armhf")
+ARCHS="linux/amd64,linux/arm64"
 
 # Read Dockerfile from the first argument
 DOCKERFILE_PATH=$1
@@ -25,21 +25,11 @@ if [[ ! -f "$DOCKERFILE_PATH" ]]; then
 fi
 
 # Loop through each architecture to build and push
-IMAGES_LIST=()  # Array to store images for manifest
-for DOCKER_ARCH in "${ARCHS[@]}"; do
-    IMAGE_FULL_NAME="$DOCKER_REGISTRY:$TAG"
-  
-    # Use Buildx to build the image for the specified architecture
-    docker buildx build --platform linux/"$DOCKER_ARCH" \
-        --file "$DOCKERFILE_PATH" \
-        --progress=plain \
-        --tag "$IMAGE_FULL_NAME" \
-        . --push  # set build context to src/templates
+IMAGE_FULL_NAME="$DOCKER_REGISTRY:$TAG"
 
-    IMAGES_LIST+=("$IMAGE_FULL_NAME")
-done
-
-# Create the manifest list under the same repository
-MANIFEST_TAG="$DOCKER_REGISTRY:latest"
-docker buildx imagetools create --tag "$MANIFEST_TAG" "${IMAGES_LIST[@]}"
-
+# Use Buildx to build the image for the specified architecture
+docker buildx build --platform "$ARCHS" \
+    --file "$DOCKERFILE_PATH" \
+    --progress=plain \
+    --tag "$IMAGE_FULL_NAME" \
+    . --push
