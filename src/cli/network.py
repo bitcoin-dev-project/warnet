d05fa655a24a2c7e80f25a7101e82234fd14ce7b
willcl-ark <will@256k1.dev>
2024-02-07 14:33:39 +0000

    server: move warnet creation back into a thread

--- src/cli/network.py
+++ src/cli/network.py
@@ -63,12 +63,7 @@ def start(graph_file: Path, force: bool, network: str):
         "network_from_file",
         {"graph_file": encoded_graph_file, "force": force, "network": network},
     )
-    assert isinstance(result, dict), "Result is not a dict"  # Make mypy happy
-    if not isinstance(result, dict):
-        print(
-            f"Error. Expected dict, got {type(result)}. May indicate mismatch between RPC and CLI"
-        )
-        sys.exit(1)
+    assert isinstance(result, dict)
     print_repr(result)
 
 
