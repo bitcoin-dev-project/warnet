cfbffa5d1454e150ce5e00909db4180c09597ce9
Max Edwards <youwontforgetthis@gmail.com>
2024-02-13 18:36:05 +0000

    add prometheus helm chart

--- src/templates/k8s/install_logging.sh
+++ src/templates/k8s/install_logging.sh
@@ -4,6 +4,7 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 helm upgrade --install --namespace warnet-logging --create-namespace --values "${SCRIPT_DIR}/loki/values.yaml" loki grafana/loki
