3be61a9e053f249680e98c5a1923784949edcac6
Max Edwards <youwontforgetthis@gmail.com>
2024-02-14 22:50:23 +0000

    logs only collected if graph requests so

--- src/templates/k8s/install_logging.sh
+++ src/templates/k8s/install_logging.sh
@@ -8,6 +8,6 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 helm upgrade --install --namespace warnet-logging --create-namespace --values "${SCRIPT_DIR}/loki/values.yaml" loki grafana/loki
-helm upgrade --install --namespace warnet-logging promtail grafana/promtail
+helm upgrade --install --namespace warnet-logging promtail grafana/promtail --values "${SCRIPT_DIR}/promtail/values.yaml"
 helm upgrade --install --namespace warnet-logging prometheus prometheus-community/kube-prometheus-stack --namespace warnet-logging --set grafana.enabled=false
 helm upgrade --install --namespace warnet-logging loki-grafana grafana/grafana --values "${SCRIPT_DIR}/grafana/values.yaml"
