8df6caae479343344dc7e6b91838880176be90d1
willcl-ark <will@256k1.dev>
2024-03-28 16:48:13 +0000

    add run_file command

--- src/rust-cli/scenarios.rs
+++ src/rust-cli/scenarios.rs
@@ -1,21 +1,29 @@
 use crate::rpc_call::make_rpc_call;
 use anyhow::Context;
+use base64::{engine::general_purpose, Engine as _};
 use clap::Subcommand;
 use jsonrpsee::core::params::ObjectParams;
 use prettytable::{row, Table};
+use std::path::PathBuf;
 
 #[derive(Subcommand, Debug)]
 pub enum ScenarioCommands {
     /// List available scenarios in the Warnet Test Framework
     Available {},
+    /// Run a scenario from remote repository with <name>
     Run {
         scenario: String,
         additional_args: Vec<String>,
     },
-    Active {},
-    Stop {
-        pid: u64,
+    /// Run a local scenario <file> by sending it to the server
+    RunFile {
+        scenario_path: PathBuf,
+        additional_args: Vec<String>,
     },
+    /// List active scenarios
+    Active {},
+    /// Stop a scenario with <PID>
+    Stop { pid: u64 },
 }
 
 pub async fn handle_scenario_command(
@@ -64,6 +72,25 @@ pub async fn handle_scenario_command(
                 .context("Failed to run scenario")?;
             println!("{:?}", data);
         }
+
+        ScenarioCommands::RunFile {
+            scenario_path,
+            additional_args,
+        } => {
+            let file_contents =
+                std::fs::read(scenario_path).context("Failed to read scenario file")?;
+            let scenario_base64 = general_purpose::STANDARD.encode(file_contents);
+            params
+                .insert("scenario_base64", scenario_base64)
+                .context("Add scenario to params")?;
+            params
+                .insert("additional_args", additional_args)
+                .context("Add additional_args to params")?;
+            let data = make_rpc_call("scenarios_run_file", params)
+                .await
+                .context("Failed to run scenario")?;
+            println!("{:?}", data);
+        }
         ScenarioCommands::Active {} => {
             let data = make_rpc_call("scenarios_list_running", params)
                 .await
@@ -102,7 +129,11 @@ pub async fn handle_scenario_command(
             let data = make_rpc_call("scenarios_stop", params)
                 .await
                 .context("Failed to stop running scenario")?;
-            println!("{:?}", data);
+            if let serde_json::Value::String(message) = data {
+                println!("{}", message);
+            } else {
+                println!("Unexpected response format.");
+            }
         }
     }
     Ok(())
