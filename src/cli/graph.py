bb947afa345ae9558b507cec46c4c8d9893d585d
Matthew Zipkin <pinheadmz@gmail.com>
2024-04-05 14:06:40 -0400

    rpc: add --cb option for circuitbreaker image in import-json

--- src/cli/graph.py
+++ src/cli/graph.py
@@ -38,7 +38,8 @@ def create(number: int, outfile: Path, version: str, bitcoin_conf: Path, random:
 @graph.command()
 @click.argument("infile", type=click.Path())
 @click.option("--outfile", type=click.Path())
-def import_json(infile: Path, outfile: Path):
+@click.option("--cb", type=str, default="pinheadmz/circuitbreaker:278737d")
+def import_json(infile: Path, outfile: Path, cb: str):
     """
     Create a cycle graph with nodes imported from lnd `describegraph` JSON file,
     and additionally include 7 extra random outbounds per node. Include lightning
@@ -55,7 +56,7 @@ def import_json(infile: Path, outfile: Path):
     for index, n in enumerate(graph.nodes()):
         graph.nodes[n]["bitcoin_config"] = f"-uacomment=tank{index:06}"
         graph.nodes[n]["ln"] = "lnd"
-        graph.nodes[n]["ln_cb_image"] = "pinheadmz/circuitbreaker:278737d"
+        graph.nodes[n]["ln_cb_image"] = cb
         graph.nodes[n]["ln_config"] = "--protocol.wumbo-channels"
 
     # Save a map of LN pubkey -> Tank index
