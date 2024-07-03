f64ca3317ef08c9a15d1b8f92f7521b96f3727a6
willcl-ark <will@256k1.dev>
2024-07-03 23:40:42 +0100

    WIP: temp support custom tag

--- src/warnet/utils.py
+++ src/warnet/utils.py
@@ -21,7 +21,7 @@ from warnet import SRC_DIR
 logger = logging.getLogger("utils")
 
 
-SUPPORTED_TAGS = ["27.0", "26.0", "25.1", "24.2", "23.2", "22.2"]
+SUPPORTED_TAGS = ["27.1-exe", "27.0", "26.0", "25.1", "24.2", "23.2", "22.2"]
 DEFAULT_TAG = SUPPORTED_TAGS[0]
 WEIGHTED_TAGS = [
     tag for index, tag in enumerate(reversed(SUPPORTED_TAGS)) for _ in range(index + 1)
