8c52b376cde8568eaa3345b9a39b33e1600eea77
willcl-ark <will@256k1.dev>
2024-07-05 13:43:24 +0100

    image: build by commit hash, not branch

--- src/cli/image.py
+++ src/cli/image.py
@@ -12,17 +12,17 @@ def image():
 
 @image.command()
 @click.option("--repo", required=True, type=str)
-@click.option("--branch", required=True, type=str)
+@click.option("--commit-sha", required=True, type=str)
 @click.option("--registry", required=True, type=str)
 @click.option("--tag", required=True, type=str)
 @click.option("--build-args", required=False, type=str)
 @click.option("--arches", required=False, type=str)
 @click.option("--action", required=False, type=str)
-def build(repo, branch, registry, tag, build_args, arches, action="load"):
+def build(repo, commit_sha, registry, tag, build_args, arches, action="load"):
     """
-    Build bitcoind and bitcoin-cli from <repo>/<branch> as <registry>:<tag>.
+    Build bitcoind and bitcoin-cli from <repo> at <commit_sha> as <registry>:<tag>.
     Optionally deploy to remote registry using --action=push, otherwise image is loaded to local registry.
     """
-    res = build_image(repo, branch, registry, tag, build_args, arches, action)
+    res = build_image(repo, commit_sha, registry, tag, build_args, arches, action)
     if not res:
         sys.exit(1)
