1c1008800fac637da83c5a0ca9278d181f378a04
Matthew Zipkin <pinheadmz@gmail.com>
2024-08-02 15:47:43 -0400

    grafana: skip login page, no password required

--- resources/scripts/connect_logging.sh
+++ resources/scripts/connect_logging.sh
@@ -3,6 +3,6 @@ set -e
 
 POD_NAME=$(kubectl get pods --namespace warnet-logging -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=loki-grafana" -o jsonpath="{.items[0].metadata.name}")
 
-echo "Go to http://localhost:3000 and login with the username 'admin' and the password 'password' to see your logs"
+echo "Go to http://localhost:3000"
 
 kubectl --namespace warnet-logging port-forward "${POD_NAME}" 3000
