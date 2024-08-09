c8cada2bad354fda07adc6c529e45e8c3cdc0a72
willcl-ark <will@256k1.dev>
2024-08-09 12:08:11 +0100

    cli: image: enable specifying full image tags

--- src/warnet/image.py
+++ src/warnet/image.py
@@ -7,22 +7,27 @@ from .image_build import build_image
 
 @click.group(name="image")
 def image():
-    """Build a a custom Warnet Bitcoin Core image"""
+    """Build a custom Warnet Bitcoin Core image"""
 
 
 @image.command()
 @click.option("--repo", required=True, type=str)
 @click.option("--commit-sha", required=True, type=str)
 @click.option("--registry", required=True, type=str)
-@click.option("--tag", required=True, type=str)
+@click.option(
+    "--tags",
+    required=True,
+    type=str,
+    help="Comma-separated list of full tags including image names",
+)
 @click.option("--build-args", required=False, type=str)
 @click.option("--arches", required=False, type=str)
 @click.option("--action", required=False, type=str, default="load")
-def build(repo, commit_sha, registry, tag, build_args, arches, action):
+def build(repo, commit_sha, registry, tags, build_args, arches, action):
     """
-    Build bitcoind and bitcoin-cli from <repo> at <commit_sha> as <registry>:<tag>.
+    Build bitcoind and bitcoin-cli from <repo> at <commit_sha> with the specified <tags>.
     Optionally deploy to remote registry using --action=push, otherwise image is loaded to local registry.
     """
-    res = build_image(repo, commit_sha, registry, tag, build_args, arches, action)
+    res = build_image(repo, commit_sha, registry, tags, build_args, arches, action)
     if not res:
         sys.exit(1)
