a9e829d875cfaf957c2da4a51c5c079f571c3c52
Grant <mpls@posteo.net>
2024-08-16 09:51:59 -0500

    add kubectl and helm checks
    Allow the setup-minikube command to check for helm and kubectl because minikube will want to use
    kubectl right away, and helm is needed for logging later on.

--- resources/scripts/setup_minikube.sh
+++ resources/scripts/setup_minikube.sh
@@ -72,6 +72,24 @@ else
     ERROR_CODE=127
 fi
 
+kubectl_path=$(command -v kubectl || true)
+if [ -n "$kubectl_path" ]; then
+    print_partial_message " ⭐️ Found " "kubectl" ": $kubectl_path " "$BOLD"
+else
+    print_partial_message " 💥 Could not find " "kubectl" ". Please follow this link to install it..." "$BOLD"
+    print_message "" "   https://kubernetes.io/docs/tasks/tools/" "$BOLD"
+    ERROR_CODE=127
+fi
+
+helm_path=$(command -v helm || true)
+if [ -n "$helm_path" ]; then
+    print_partial_message " ⭐️ Found " "helm" ": $helm_path" "$BOLD"
+else
+    print_partial_message " 💥 Could not find " "helm" ". Please follow this link to install it..." "$BOLD"
+    print_message "" "   https://helm.sh/docs/intro/install/" "$BOLD"
+    ERROR_CODE=127
+fi
+
 if [ $ERROR_CODE -ne 0 ]; then
     print_message "" "There were errors in the setup process. Please fix them and try again." "$BOLD"
     exit $ERROR_CODE
