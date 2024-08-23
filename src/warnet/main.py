8bc888df181fd6c1dc78be802bff6a7a1fb64eb7
willcl-ark <will@256k1.dev>
2024-08-23 10:09:58 +0200

    remove dup help command
    `--help` exists natively via Click, use it.

--- src/warnet/main.py
+++ src/warnet/main.py
@@ -29,42 +29,6 @@ cli.add_command(scenarios)
 cli.add_command(admin)
 
 
-@cli.command(name="help")
-@click.argument("commands", required=False, nargs=-1)
-@click.pass_context
-def help_command(ctx, commands):
-    """
-    Display help information for the given [command] (and sub-command).
-    If no command is given, display help for the main CLI.
-    """
-    if not commands:
-        # Display help for the main CLI
-        richprint(ctx.parent.get_help())
-        return
-
-    # Recurse down the subcommands, fetching the command object for each
-    cmd_obj = cli
-    for command in commands:
-        cmd_obj = cmd_obj.get_command(ctx, command)
-        if cmd_obj is None:
-            richprint(f'Unknown command "{command}" in {commands}')
-            return
-        ctx = click.Context(cmd_obj, info_name=command, parent=ctx)
-
-    if cmd_obj is None:
-        richprint(f"Unknown command: {commands}")
-        return
-
-    # Get the help info
-    help_info = cmd_obj.get_help(ctx).strip()
-    # Get rid of the duplication
-    help_info = help_info.replace("Usage: warcli help [COMMANDS]...", "Usage: warcli", 1)
-    richprint(help_info)
-
-
-cli.add_command(help_command)
-
-
 @cli.command()
 def setup():
     """Check Warnet requirements are installed"""
