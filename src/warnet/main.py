0427a2f2cce144ff5df1f4e777a9b09d83d440f7
josie <josibake@protonmail.com>
2024-09-09 14:02:39 +0200

    add logs back to cli (#554)

--- src/warnet/main.py
+++ src/warnet/main.py
@@ -2,7 +2,7 @@ import click
 
 from .admin import admin
 from .bitcoin import bitcoin
-from .control import down, run, stop
+from .control import down, logs, run, stop
 from .deploy import deploy
 from .graph import graph
 from .image import image
@@ -19,12 +19,13 @@ def cli():
 cli.add_command(admin)
 cli.add_command(auth)
 cli.add_command(bitcoin)
-cli.add_command(new)
 cli.add_command(deploy)
 cli.add_command(down)
 cli.add_command(graph)
 cli.add_command(image)
 cli.add_command(init)
+cli.add_command(logs)
+cli.add_command(new)
 cli.add_command(run)
 cli.add_command(setup)
 cli.add_command(status)
