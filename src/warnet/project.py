aeb81ebe1fa8e4e1bba6e55c9f9aed4ec5d3304a
Matthew Zipkin <pinheadmz@gmail.com>
2024-09-10 03:16:19 -0400

    setup: add "No Backend" option (#556)
    * setup: add "No Backend" option
    
    * reccomend warnet auth

--- src/warnet/project.py
+++ src/warnet/project.py
@@ -209,7 +209,11 @@ def setup():
             inquirer.List(
                 "platform",
                 message=click.style("Which platform would you like to use?", fg="blue", bold=True),
-                choices=["Minikube", "Docker Desktop"],
+                choices=[
+                    "Minikube",
+                    "Docker Desktop",
+                    "No Backend (Interacting with remote cluster, see `warnet auth --help`)",
+                ],
             )
         ]
         answers = inquirer.prompt(questions)
@@ -219,15 +223,13 @@ def setup():
             if answers["platform"] == "Docker Desktop":
                 check_results.append(check_installation(docker_info))
                 check_results.append(check_installation(docker_desktop_info))
-                check_results.append(check_installation(kubectl_info))
-                check_results.append(check_installation(helm_info))
             elif answers["platform"] == "Minikube":
                 check_results.append(check_installation(docker_info))
                 check_results.append(check_installation(minikube_info))
                 if is_platform_darwin():
                     check_results.append(check_installation(minikube_version_info))
-                check_results.append(check_installation(kubectl_info))
-                check_results.append(check_installation(helm_info))
+            check_results.append(check_installation(kubectl_info))
+            check_results.append(check_installation(helm_info))
         else:
             click.secho("Please re-run setup.", fg="yellow")
             sys.exit(1)
