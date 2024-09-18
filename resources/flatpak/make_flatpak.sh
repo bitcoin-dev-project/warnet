37f3f83705cbbb2fe5e5390df91ae55dd94aef53
willcl-ark <will@256k1.dev>
2024-09-18 13:11:25 +0100

    add flatpak build job

--- resources/flatpak/make_flatpak.sh
+++ resources/flatpak/make_flatpak.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
+set -ex
+
 # Pre-clean
 rm -Rf resources/flatpak/.flatpak-builder
 rm -Rf resources/flatpak/repo
 
 # Build python wheel
-rm -i -Rf build/ dist/; python3 -m build
-mv "dist/warnet-${WARNET_VERSION}-py3-none-any.whl" dist/warnet.whl
+rm -i -Rf build/ dist/
+python3 -m build
+ls -al dist
+mv dist/warnet-*-py3-none-any.whl dist/warnet.whl
 cd resources/flatpak || exit
 
 # Dump all dependencies to wheels
 python -m pip wheel --wheel-dir=wheels warnet
 
+# Setup flatpak output dir
+if [ -n "$GITHUB_REF" ]; then
+    export LOCATION=warnet_flatpak
+else
+    export LOCATION=/tmp/warnet-flatpak/
+fi
+echo Location set to: "$LOCATION"
+
+mkdir -p "$LOCATION"
+rm -Rf "${LOCATION:?}"/*
+mkdir -p "$LOCATION/repo"
+
 # Build flatpak
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install /tmp/warnet-flatpak org.bitcoindevproject.Warnet.yaml
+flatpak-builder --force-clean --user --install-deps-from=flathub --repo="${LOCATION}/repo" --install "${LOCATION}" org.bitcoindevproject.Warnet.yaml
 
 # Clean up after ourselves
 rm -Rf .flatpak-builder
-rm -Rf repo
 rm -Rf wheels
