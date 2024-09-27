39db0b557adc60f8059be09f2562f71444002378
Grant <mpls@posteo.net>
2024-09-27 18:07:34 -0500

    constants: add k8s internal namespaces
    We want to know the namespaces used internally by k8s so that we can filter them out when querying
    namespaces.
    
    We can also likely replace this logic after we create a labeling strategy for our namespaces.

--- src/warnet/constants.py
+++ src/warnet/constants.py
@@ -15,6 +15,7 @@ DEFAULT_NAMESPACE = "default"
 LOGGING_NAMESPACE = "warnet-logging"
 INGRESS_NAMESPACE = "ingress"
 WARGAMES_NAMESPACE_PREFIX = "wargames-"
+KUBE_INTERNAL_NAMESPACES = ["kube-node-lease", "kube-public", "kube-system", "kubernetes-dashboard"]
 HELM_COMMAND = "helm upgrade --install --create-namespace"
 
 BITCOINCORE_CONTAINER = "bitcoincore"
