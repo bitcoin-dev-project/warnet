44c3c1bba9172e80becfe5e2ecc5a0d34eb603a5
willcl-ark <will@256k1.dev>
2025-03-31 09:25:02 +0100

    remove unused docker_registry param

--- src/warnet/image.py
+++ src/warnet/image.py
@@ -13,7 +13,6 @@ def image():
 @image.command()
 @click.option("--repo", required=True, type=str)
 @click.option("--commit-sha", required=True, type=str)
-@click.option("--registry", required=True, type=str)
 @click.option(
     "--tags",
     required=True,
@@ -24,10 +23,12 @@ def image():
 @click.option("--arches", required=False, type=str)
 @click.option("--action", required=False, type=str, default="load")
 def build(repo, commit_sha, registry, tags, build_args, arches, action):
+    """Build a Bitcoin Core Docker image with specified parameters.
+
+    \b
+    Usage:
+        warnet image build --repo bitcoin/bitcoin --commit-sha d6db87165c6dc2123a759c79ec236ea1ed90c0e3 --tags bitcoindevproject/bitcoin:v29.0-rc2 --action push
     """
-    Build bitcoind and bitcoin-cli from <repo> at <commit_sha> with the specified <tags>.
-    Optionally deploy to remote registry using --action=push, otherwise image is loaded to local registry.
-    """
-    res = build_image(repo, commit_sha, registry, tags, build_args, arches, action)
+    res = build_image(repo, commit_sha, tags, build_args, arches, action)
     if not res:
         sys.exit(1)
