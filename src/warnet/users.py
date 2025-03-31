952bfff64a438c8f7bb3d04fdbe74d379030bcc2
Grant <mpls@posteo.net>
2025-03-31 09:28:10 -0500

    handle missing key in base config

--- src/warnet/users.py
+++ src/warnet/users.py
@@ -41,7 +41,7 @@ def auth(auth_config):
     if not is_first_config:
         for category in ["clusters", "users", "contexts"]:
             if category in auth_config:
-                if not base_config[category]:
+                if category in base_config and not base_config[category]:
                     base_config[category] = []
                 merge_entries(category, base_config, auth_config)
 
