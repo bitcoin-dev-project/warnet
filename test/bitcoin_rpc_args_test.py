12bf6bd0e2aa81514f30b61ac3a9f3c134135540
Matthew Zipkin <pinheadmz@pm.me>
2025-07-22 16:01:23 -0400

    run bitcoin args test

--- test/bitcoin_rpc_args_test.py
+++ test/bitcoin_rpc_args_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import shlex
 import sys
 from pathlib import Path
@@ -6,9 +8,10 @@ from unittest.mock import patch
 # Import TestBase for consistent test structure
 from test_base import TestBase
 
+from warnet.bitcoin import _rpc
+
 # Import _rpc from warnet.bitcoin and run_command from warnet.process
 sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
-from warnet.bitcoin import _rpc
 
 # Edge cases to test
 EDGE_CASES = [
