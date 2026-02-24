4f21ae967db4e108fef3454cf0a7f323048fca9b
Matthew Zipkin <pinheadmz@pm.me>
2026-02-24 11:43:11 -0300

    test: delete PVCs on shutdown

--- test/test_base.py
+++ test/test_base.py
@@ -52,7 +52,20 @@ class TestBase:
                 session.logfile = sys.stdout
                 session.expect("Do you want to bring down the running Warnet?", timeout=30)
                 session.sendline("y")
-                session.expect("Warnet teardown process completed", timeout=300)
+
+                # Handle PVC question if asked within 5 seconds
+                outcome = session.expect(
+                    ["Do you also want to delete all PVCs", pexpect.EOF, pexpect.TIMEOUT], timeout=5
+                )
+                if outcome == 1:
+                    assert "Warnet teardown process completed" in session.before
+                    self.log.info("Warnet teardown process completed")
+                else:
+                    if outcome == 0:
+                        session.sendline("y")
+                    else:
+                        self.log.info("Didn't get delete PVC question, continuing...")
+                    session.expect("Warnet teardown process completed", timeout=300)
                 self.wait_for_all_tanks_status(target="stopped", timeout=60, interval=1)
         except Exception as e:
             self.log.error(f"Error bringing network down: {e}")
